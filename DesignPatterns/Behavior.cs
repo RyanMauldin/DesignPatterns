5ea52410da45097edfbf45e41d23d7e27fe473ee
Ryan Mauldin <ryanmauldin@msn.com>
2015-10-09 15:47:36 -0400

    More Factory Method Pattern Comments.
    Left out comments on Factory Method Pattern interfaces as well as
    created a comment fix on Behavior class mentioning interface.

--- DesignPatterns/Behavior.cs
+++ DesignPatterns/Behavior.cs
@@ -8,10 +8,8 @@ namespace DesignPatterns
     /// Object Oriented programming paradigm. Behaviors are
     /// really more of a classification in Design Pattern
     /// lingo. The classification is "Behavioral" design
-    /// patterns. However this is a nice interface to show
-    /// how to achieve a family of alorithms or the Strategy
-    /// Pattern. This is an example base class to derive from
-    /// to give an example Behavior.
+    /// patterns. This is an example base class to derive
+    /// from to give an example Behavior.
     /// </summary>
     /// <typeparam name="T">The type to operate on.</typeparam>
     public abstract class Behavior<T> :
--- DesignPatterns/Interfaces/IFactoryMethod.cs
+++ DesignPatterns/Interfaces/IFactoryMethod.cs
@@ -1,8 +1,21 @@
 namespace DesignPatterns.Interfaces
 {
+    /// <summary>
+    /// Intent - Factory methods allow the user to create and
+    /// initialize objects by delegating the work to a facotry
+    /// method. The Factory Method pattern, when paired with IOC
+    /// (Inversion of Control) pattern, is extremely powerful
+    /// for Unit Tests, where Moch data objects need to be used
+    /// instead of the real deal.
+    /// </summary>
+    /// <typeparam name="T">The Type of class to create.</typeparam>
     public interface IFactoryMethod<out T>
         where T : class, new()
     {
+        /// <summary>
+        /// The main factory method.
+        /// </summary>
+        /// <returns>A newed up instance of type T.</returns>
         T Create();
     }
 }
